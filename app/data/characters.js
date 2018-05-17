75b7ffcd81dbdca06dbbe6237feb6b647a160999
Jacob Allred <jakedude71@gmail.com>
2018-05-17 13:55:06 -0400

    Initial commit

---
+++ app/data/characters.js
@@ -0,0 +1,119 @@
+var characters = [
+  {
+    name: "Jimmy Neutron",
+    photo: "https://vignette.wikia.nocookie.net/jimmyneutron/images/5/57/Jimmy_Neutron.png/revision/latest?cb=20180219044703",
+    scores: [
+      "5",
+      "1",
+      "2",
+      "2",
+      "4",
+      "2",
+      "2",
+      "4",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Shrek",
+    photo: "https://vignette.wikia.nocookie.net/shrek/images/9/9b/GoodShrekImage.png/revision/latest?cb=20171217221732",
+    scores: [
+      "2",
+      "5",
+      "1",
+      "5",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Kool Aid Guy",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/518XGBF-a-L._SX342_.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Rasta Guy",
+    photo: "https://inst-2.cdn.shockers.de/hs_cdn/out/pictures//master/product/1/rasta-peruecke-schwarz--raggae-peruecke--piraten-peruecke--herren-peruecke--rasta-wig-black--16840.jpg",
+    scores: [
+      "3",
+      "3",
+      "5",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Cool Kid",
+    photo: "https://cdn.trendhunterstatic.com/thumbs/pizza-inspired.jpeg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Bleh Cat",
+    photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-08/31/14/campaign_images/buzzfeed-prod-fastlane01/oh-my-god-these-cats-look-so-adorably-dumb-when-t-2-5119-1472669792-3_dblbig.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "5",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+
+{
+    name: "Coffee Spill Man",
+    photo: "https://affotd.files.wordpress.com/2013/11/spilled-coffee.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "5",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+
+module.exports = characters;
